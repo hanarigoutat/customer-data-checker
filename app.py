a6ee749ea07882bf27bc578efce7336933f860b3
hanarigoutat <hana.rigoutat-ext@aroma-zone.com>
2026-05-29 14:28:44 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 import streamlit as st
 import zipfile
 import time
+import polars as pl
 
-st.title("Test lecture ZIP")
+st.title("Lecture CSV")
 
 uploaded_file = st.file_uploader(
     "Dépose ton ZIP",
@@ -11,28 +12,28 @@ uploaded_file = st.file_uploader(
 
 if uploaded_file:
 
-    progress = st.progress(0)
-    status = st.empty()
-
-    t0 = time.time()
-
-    status.write("📥 Fichier reçu")
-    progress.progress(10)
-
-    status.write("📦 Ouverture du ZIP...")
     with zipfile.ZipFile(uploaded_file) as z:
 
-        progress.progress(40)
+        csv_file = [
+            f for f in z.namelist()
+            if f.lower().endswith(".csv")
+            and "__macosx" not in f.lower()
+        ][0]
 
-        files = z.namelist()
+        st.write(f"CSV trouvé : {csv_file}")
 
-        status.write(f"📄 {len(files)} fichier(s) trouvé(s)")
-        progress.progress(70)
+        with z.open(csv_file) as f:
 
-        st.write(files)
+            start = time.time()
 
-    progress.progress(100)
+            df = pl.read_csv(
+                f,
+                columns=["Email", "Email Marketing Consent"],
+                n_rows=1000
+            )
 
-    status.write(
-        f"✅ Terminé en {time.time() - t0:.1f} secondes"
-    )
+            st.success(
+                f"1000 lignes lues en {time.time()-start:.1f}s"
+            )
+
+            st.write(df.head())
