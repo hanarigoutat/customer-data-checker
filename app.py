fafacf39ad778b2992a0fec142742449a49adeac
hanarigoutat <hana.rigoutat-ext@aroma-zone.com>
2026-05-29 12:50:51 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,19 @@
 import streamlit as st
+import requests
 
-st.title("Customer Data Checker")
+st.title("Test SharePoint")
 
-st.write("Test de connexion SharePoint")
+url = st.text_input(
+    "URL du fichier SharePoint",
+    value="https://hyteck.sharepoint.com"
+)
+
+if st.button("Tester l'accès"):
+    try:
+        r = requests.get(url, timeout=20)
+
+        st.write("Status code :", r.status_code)
+        st.write("Headers :", dict(r.headers))
+
+    except Exception as e:
+        st.error(str(e))
