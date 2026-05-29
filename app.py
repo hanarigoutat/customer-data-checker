79cdac89ab393476a87705b5fad352fda3a0c7f3
hanarigoutat <hana.rigoutat-ext@aroma-zone.com>
2026-05-29 13:08:18 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,12 @@
 import streamlit as st
-import zipfile
 
-st.title("Test ZIP")
+st.title("Test upload")
 
 uploaded_file = st.file_uploader(
-    "Dépose un ZIP",
+    "Dépose ton ZIP",
     type=["zip"]
 )
 
 if uploaded_file:
-
-    st.success("ZIP uploadé")
-
-    with zipfile.ZipFile(uploaded_file) as z:
-
-        csv_files = [
-            f for f in z.namelist()
-            if f.lower().endswith(".csv")
-        ]
-
-        st.write("CSV trouvés :")
-        st.write(csv_files)
+    st.success("Fichier reçu")
+    st.write(f"Taille : {uploaded_file.size / 1024 / 1024:.1f} MB")
